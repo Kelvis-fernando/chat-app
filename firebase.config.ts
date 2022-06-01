fe68b61c204676f6c7c358f16362cd85d45cd7f2
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-06-01 18:10:07 -0300

    past for .env the variable for access firebase

--- firebase.config.ts
+++ firebase.config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyAB761Y3rdis3EFhglDm55lKuUAj8OSoCo",
-   authDomain: "chat-app-1b664.firebaseapp.com",
-   projectId: "chat-app-1b664",
-   storageBucket: "chat-app-1b664.appspot.com",
-   messagingSenderId: "187865427589",
-   appId: "1:187865427589:web:7ef60d276ab167692343ec",
-   measurementId: "G-CRL4BX6H1Y",
+   apiKey: process.env.apiKey,
+   authDomain: process.env.authDomain,
+   projectId: process.env.projectId,
+   storageBucket: process.env.storageBucket,
+   messagingSenderId: process.env.messagingSenderId,
+   appId: process.env.appId,
+   measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   env: {
+      apiKey: "AIzaSyAB761Y3rdis3EFhglDm55lKuUAj8OSoCo",
+      authDomain: "chat-app-1b664.firebaseapp.com",
+      projectId: "chat-app-1b664",
+      storageBucket: "chat-app-1b664.appspot.com",
+      messagingSenderId: "187865427589",
+      appId: "1:187865427589:web:7ef60d276ab167692343ec",
+      measurementId: "G-CRL4BX6H1Y",
+   },
    reactStrictMode: true,
 };
 
 module.exports = nextConfig;
-
