e73fd31a41bf4ecd4ce40cce10986dcfd8c92d64
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-08-01 10:26:15 -0300

    Created mock for contacts, and made a render to object

--- src/components/chat/Contacts.tsx
+++ src/components/chat/Contacts.tsx
@@ -3,22 +3,62 @@ import Image from "next/image";
 import styles from "../../styles/chat/Contacts.module.scss";
 
 const Contacts: NextPage = () => {
-   return (
-      <div className={styles.contacts}>
-         <Image
-            src="/perfil.png"
-            width={50}
-            height={50}
-            className="rounded"
-            alt="Phoro from perfil"
-         />
-         <div className={styles.body}>
-            <p>Kelvis Fernando</p>
-            <p>Olá</p>
+   const contacts = [
+      {
+         name: "Kelvis Fernando",
+         lastMessage: "Ola",
+         timeLastMessage: "1h",
+         photo: "/perfil.png",
+      },
+      {
+         name: "Fernando Delapena",
+         lastMessage: "Tudo bem",
+         timeLastMessage: "3h",
+         photo: "/perfil.png",
+      },
+      {
+         name: "Stefany Fernanda",
+         lastMessage: "Vai",
+         timeLastMessage: "30min",
+         photo: "/perfil.png",
+      },
+      {
+         name: "Mae",
+         lastMessage: "Te amo <3",
+         timeLastMessage: "6h",
+         photo: "/perfil.png",
+      },
+      {
+         name: "Pai",
+         lastMessage: "Te amo <3 <3",
+         timeLastMessage: "6h",
+         photo: "/perfil.png",
+      },
+   ];
+   function renderMessages() {
+      return contacts.map((contact, index) => (
+         <div className={styles.contacts} key={index}>
+            <Image
+               src={contact.photo}
+               width={50}
+               height={50}
+               className="rounded"
+               alt="Photo from perfil"
+            />
+            <div className={styles.body}>
+               <p>{contact.name}</p>
+               <p>{contact.lastMessage}</p>
+            </div>
+            <span className={styles.timing}>{contact.timeLastMessage}</span>
          </div>
-         <span className={styles.timing}>1h</span>
-      </div>
-   );
+      ))
+   }
+   
+   return (
+      <>
+         {renderMessages()}
+      </>
+   )
 };
 
 export default Contacts;
