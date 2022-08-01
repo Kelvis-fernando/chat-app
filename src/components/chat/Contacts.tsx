00fe0dc1ba9696df2dc20e53fd17edc3ef1fd024
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-08-01 10:39:43 -0300

    Added routes with hash to message area

--- src/components/chat/Contacts.tsx
+++ src/components/chat/Contacts.tsx
@@ -1,43 +1,54 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../../styles/chat/Contacts.module.scss";
 
 const Contacts: NextPage = () => {
    const contacts = [
       {
+         id: "hsdjagdn",
          name: "Kelvis Fernando",
          lastMessage: "Ola",
          timeLastMessage: "1h",
          photo: "/perfil.png",
       },
       {
+         id: "hsdjagdn",
          name: "Fernando Delapena",
          lastMessage: "Tudo bem",
          timeLastMessage: "3h",
          photo: "/perfil.png",
       },
       {
+         id: "hsdjagdn",
          name: "Stefany Fernanda",
          lastMessage: "Vai",
          timeLastMessage: "30min",
          photo: "/perfil.png",
       },
       {
+         id: "hsdjagdn",
          name: "Mae",
          lastMessage: "Te amo <3",
          timeLastMessage: "6h",
          photo: "/perfil.png",
       },
       {
+         id: "hsdjagdn",
          name: "Pai",
          lastMessage: "Te amo <3 <3",
          timeLastMessage: "6h",
          photo: "/perfil.png",
       },
    ];
+
+   function setContactMessage(id: string) {
+      return window.location.href = "/messagearea/" + id;
+   }
+
    function renderMessages() {
       return contacts.map((contact, index) => (
-         <div className={styles.contacts} key={index}>
+         <div className={styles.contacts} key={index} onClick={() => setContactMessage(contact.id)}>
             <Image
                src={contact.photo}
                width={50}
